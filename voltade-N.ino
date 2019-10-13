0372f052f78d60ce7d1a1c668697c0c479464811
Abel C Dixon <abelcheruvathoor@gmail.com>
2019-10-13 14:22:53 +0530

    Add files via upload

---
+++ voltade-N.ino
@@ -0,0 +1,32 @@
+  #include <SoftwareSerial.h>
+#include <LiquidCrystal.h>   // LIBRARY TO ACCESS THE LCD DISPLAY
+LiquidCrystal lcd( 4, 5, 6, 7,8 ,9 );
+float input_volt = 0.0; 
+float temp=0.0;
+float r1=10000.0;    //r1 value
+float r2=100000.0;      //r2 value
+
+   
+void setup()
+{
+   Serial.begin(9600);     //  opens serial port, sets data rate to 9600 bps
+   lcd.begin(16, 2);       //// set up the LCD's number of columns and rows
+   lcd.print("DC DIGI VOLTMETER");
+}
+
+void loop()
+{
+    int analogvalue = analogRead(A0);
+    temp = (analogvalue * 5.0) / 1024.0;       // FORMULA USED TO CONVERT THE VOLTAGE
+    input_volt =( temp / (r2/(r1+r2)))*100;    // actual value of the form 0.01,so multiplied with 100
+if (input_volt <1) //threshold voltage
+   {
+     input_volt=0.0;
+   } 
+    Serial.print("v= ");                 // prints the voltage value in the serial monitor
+    Serial.println(input_volt );
+   // lcd.setCursor(0, 1);
+   // lcd.print("Voltage= ");               // prints the voltage value in the LCD display  
+   // lcd.print(input_volt);
+    delay(300);
+}
