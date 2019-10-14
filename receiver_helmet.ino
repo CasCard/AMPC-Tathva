4736dbf4b6aa08a46aad789d53078050327cf2a7
Abel C Dixon <abelcheruvathoor@gmail.com>
2019-10-15 00:24:07 +0530

    Add files via upload

--- receiver_helmet.ino
+++ receiver_helmet.ino
@@ -1,34 +1,49 @@
+/*
+    DIY Arduino based RC Transmitter Project
+              == Receiver Code ==
+  by Dejan Nedelkovski, www.HowToMechatronics.com
+  Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
 #include <SPI.h>
-#include <RF24.h>
 #include <nRF24L01.h>
-
-const int maxMsgLength=64;
-char msg[maxMsgLength];
-
-RF24 radio(7,8);
-const uint64_t pipe=1;
+#include <RF24.h>
+RF24 radio(7, 8);   // nRF24L01 (CE, CSN)
+const byte address[6] = "00001";
+unsigned long lastReceiveTime = 0;
+unsigned long currentTime = 0;
+// Max size of this struct is 32 bytes - NRF24L01 buffer limit
+struct Data_Package {
+  float voltage;
+};
+Data_Package data; //Create a variable with the above structure
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin(9600);
-radio.begin();
-radio.setPALevel(RF24_PA_MIN);
-Serial.println("Started !");
-radio.openReadingPipe(1,pipe);
-Serial.println("Opened !");
-
-radio.startListening();
-Serial.println("Listening..");
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setAutoAck(false);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setPALevel(RF24_PA_LOW);
+  radio.startListening(); //  Set the module as receiver
+  resetData();
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-if(radio.available()){
-  Serial.println("Available !");
-  radio.read(msg,maxMsgLength - 1);
-  Serial.println("Read: "+String(msg));
-}else{
-  Serial.println("Not Available");
+  // Check whether there is data to be received
+  if (radio.available()) {
+    radio.read(&data, sizeof(Data_Package)); // Read the whole data and store it into the 'data' structure
+    lastReceiveTime = millis(); // At this moment we have received the data
+  }
+  // Check whether we keep receving data, or we have a connection between the two modules
+  currentTime = millis();
+  if ( currentTime - lastReceiveTime > 1000 ) { // If current time is more then 1 second since we have recived the last data, that means we have lost connection
+    resetData(); // If connection is lost, reset the data. It prevents unwanted behavior, for example if a drone has a throttle up and we lose connection, it can keep flying unless we reset the values
+  }
+  // Print the data in the Serial Monitor
+  Serial.print("Voltage");
+  Serial.println(data.voltage);
+  delay(500);
+  
 }
-delay(2500);
-Serial.println("Wait...");
+void resetData() {
+  // Reset the values when there is no radio connection - Set initial default values
+  float voltage = 0.0;
 }
